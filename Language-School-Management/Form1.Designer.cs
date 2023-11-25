8dd14dbf9f4f9a96b2b2ab634c5d3b39e32c5020
TheRoot <TheRoot@gmail.com>
2023-11-25 09:55:18 +0330

    sgdg

--- Language-School-Management/Form1.Designer.cs
+++ Language-School-Management/Form1.Designer.cs
@@ -46,10 +46,10 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(140, 12);
+            this.button1.Location = new System.Drawing.Point(140, 33);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(203, 44);
-            this.button1.TabIndex = 1;
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             // 
