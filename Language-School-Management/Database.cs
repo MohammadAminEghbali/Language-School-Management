ef1de8f038c698759f36f66acd14c57c570dd6a9
TheRoot <TheRoot@gmail.com>
2023-11-30 14:34:32 +0330

    Added SQLite Database file and created Database.cs new method: AddStudent for students form

---
+++ Language-School-Management/Database.cs
@@ -0,0 +1,68 @@
+﻿using System.Data.SQLite;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace Language_School_Management
+{
+    public class Database
+    {
+        private static SQLiteConnection conn;
+        public Database()
+        {
+            conn = new SQLiteConnection("Data source=data.db");
+            conn.Open();
+            using (SQLiteCommand cmd = conn.CreateCommand())
+            {
+                cmd.CommandText = @"
+                    CREATE TABLE IF NOT EXISTS students (
+                        firstName TEXT, lastName TEXT, fatherName TEXT, nCode TEXT, birthDate TEXT,
+                        phoneNumber TEXT, homePhone TEXT, parentPhone TEXT, homeAddress TEXT
+                    );
+
+                    CREATE TABLE IF NOT EXISTS teachers (name TEXT);
+                ";
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public static void AddStudent(
+            string firstName,
+            string lastName,
+            string fatherName,
+            string nCode,
+            string birthDate,
+            string phoneNumber,
+            string homePhone,
+            string parentPhone,
+            string homeAddress)
+        {
+            using (SQLiteCommand cmd = conn.CreateCommand())
+            {
+                cmd.CommandText = @"
+                    INSERT INTO students VALUES(
+                        @firstName, @lastName, @fatherName, @nCode, @birthDate, @phoneNumber, @homePhone, @parentPhone, @homeAddress
+                    );
+                ";
+
+                cmd.Parameters.AddWithValue("firstName", firstName);
+                cmd.Parameters.AddWithValue("lastName", lastName);
+                cmd.Parameters.AddWithValue("fatherName", fatherName);
+                cmd.Parameters.AddWithValue("nCode", nCode);
+                cmd.Parameters.AddWithValue("birthDate", birthDate);
+                cmd.Parameters.AddWithValue("phoneNumber", phoneNumber);
+                cmd.Parameters.AddWithValue("homePhone", homePhone);
+                cmd.Parameters.AddWithValue("parentPhone", parentPhone);
+                cmd.Parameters.AddWithValue("homeAddress", homeAddress);
+
+                /*var parameters = new { firstName, lastName, fatherName, birthDate, phoneNumber, homePhone, parentPhone, homeAddress };
+
+                foreach (PropertyInfo parameter in parameters.GetType().GetProperties())
+                {
+                    cmd.Parameters.AddWithValue(parameter.Name, parameter.GetValue(parameters, null));
+                }*/
+
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
--- Language-School-Management/studentsManageForm.Designer.cs
+++ Language-School-Management/studentsManageForm.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.lblRegDate = new System.Windows.Forms.Label();
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.lblAddress = new System.Windows.Forms.Label();
@@ -40,7 +41,6 @@
             this.IdBox = new System.Windows.Forms.TextBox();
             this.lblFullName = new System.Windows.Forms.Label();
             this.FullNameBox = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
@@ -49,9 +49,10 @@
             // 
             this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 23);
+            this.dataGridView1.Location = new System.Drawing.Point(16, 28);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(693, 430);
+            this.dataGridView1.Size = new System.Drawing.Size(924, 529);
             this.dataGridView1.TabIndex = 0;
             // 
             // groupBox1
@@ -68,107 +69,121 @@
             this.groupBox1.Controls.Add(this.lblFullName);
             this.groupBox1.Controls.Add(this.FullNameBox);
             this.groupBox1.Font = new System.Drawing.Font("Vazir", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
-            this.groupBox1.Location = new System.Drawing.Point(725, 23);
+            this.groupBox1.Location = new System.Drawing.Point(967, 28);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.groupBox1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.groupBox1.Size = new System.Drawing.Size(515, 360);
+            this.groupBox1.Size = new System.Drawing.Size(687, 443);
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "ثبت زبان آموز";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(337, 367);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 38);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "ثبت";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // lblRegDate
             // 
-            this.lblRegDate.Location = new System.Drawing.Point(359, 248);
+            this.lblRegDate.Location = new System.Drawing.Point(479, 305);
+            this.lblRegDate.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblRegDate.Name = "lblRegDate";
-            this.lblRegDate.Size = new System.Drawing.Size(131, 29);
+            this.lblRegDate.Size = new System.Drawing.Size(175, 36);
             this.lblRegDate.TabIndex = 10;
             this.lblRegDate.Text = "تاریخ";
             // 
             // dateTimePicker1
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(33, 242);
+            this.dateTimePicker1.Location = new System.Drawing.Point(44, 298);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(305, 32);
+            this.dateTimePicker1.Size = new System.Drawing.Size(405, 32);
             this.dateTimePicker1.TabIndex = 2;
             // 
             // lblAddress
             // 
-            this.lblAddress.Location = new System.Drawing.Point(359, 194);
+            this.lblAddress.Location = new System.Drawing.Point(479, 239);
+            this.lblAddress.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblAddress.Name = "lblAddress";
-            this.lblAddress.Size = new System.Drawing.Size(131, 29);
+            this.lblAddress.Size = new System.Drawing.Size(175, 36);
             this.lblAddress.TabIndex = 7;
             this.lblAddress.Text = "آدرس";
             // 
             // lblPhone
             // 
-            this.lblPhone.Location = new System.Drawing.Point(359, 137);
+            this.lblPhone.Location = new System.Drawing.Point(479, 169);
+            this.lblPhone.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblPhone.Name = "lblPhone";
-            this.lblPhone.Size = new System.Drawing.Size(131, 29);
+            this.lblPhone.Size = new System.Drawing.Size(175, 36);
             this.lblPhone.TabIndex = 6;
             this.lblPhone.Text = "تلفن همراه";
             // 
             // AddressBox
             // 
-            this.AddressBox.Location = new System.Drawing.Point(33, 191);
+            this.AddressBox.Location = new System.Drawing.Point(44, 235);
+            this.AddressBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.AddressBox.Name = "AddressBox";
-            this.AddressBox.Size = new System.Drawing.Size(305, 32);
+            this.AddressBox.Size = new System.Drawing.Size(405, 32);
             this.AddressBox.TabIndex = 5;
             // 
             // PhoneBox
             // 
-            this.PhoneBox.Location = new System.Drawing.Point(33, 134);
+            this.PhoneBox.Location = new System.Drawing.Point(44, 165);
+            this.PhoneBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.PhoneBox.Name = "PhoneBox";
-            this.PhoneBox.Size = new System.Drawing.Size(305, 32);
+            this.PhoneBox.Size = new System.Drawing.Size(405, 32);
             this.PhoneBox.TabIndex = 4;
             // 
             // lblID
             // 
-            this.lblID.Location = new System.Drawing.Point(386, 85);
+            this.lblID.Location = new System.Drawing.Point(515, 105);
+            this.lblID.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblID.Name = "lblID";
-            this.lblID.Size = new System.Drawing.Size(104, 29);
+            this.lblID.Size = new System.Drawing.Size(139, 36);
             this.lblID.TabIndex = 3;
             this.lblID.Text = "کد ملی";
             // 
             // IdBox
             // 
-            this.IdBox.Location = new System.Drawing.Point(33, 82);
+            this.IdBox.Location = new System.Drawing.Point(44, 101);
+            this.IdBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.IdBox.Name = "IdBox";
-            this.IdBox.Size = new System.Drawing.Size(305, 32);
+            this.IdBox.Size = new System.Drawing.Size(405, 32);
             this.IdBox.TabIndex = 2;
             // 
             // lblFullName
             // 
-            this.lblFullName.Location = new System.Drawing.Point(359, 34);
+            this.lblFullName.Location = new System.Drawing.Point(479, 42);
+            this.lblFullName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblFullName.Name = "lblFullName";
-            this.lblFullName.Size = new System.Drawing.Size(131, 29);
+            this.lblFullName.Size = new System.Drawing.Size(175, 36);
             this.lblFullName.TabIndex = 1;
             this.lblFullName.Text = "نام و نام خانوادگی";
             // 
             // FullNameBox
             // 
-            this.FullNameBox.Location = new System.Drawing.Point(33, 31);
+            this.FullNameBox.Location = new System.Drawing.Point(44, 38);
+            this.FullNameBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.FullNameBox.Name = "FullNameBox";
-            this.FullNameBox.Size = new System.Drawing.Size(305, 32);
+            this.FullNameBox.Size = new System.Drawing.Size(405, 32);
             this.FullNameBox.TabIndex = 0;
             this.FullNameBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(253, 298);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(85, 31);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "ثبت";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
             // studentsManageForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.ClientSize = new System.Drawing.Size(1266, 783);
+            this.ClientSize = new System.Drawing.Size(1688, 964);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "studentsManageForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "مدیریت زبان آموزان";
